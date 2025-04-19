f692ce1dbf2564fc92276a2dbf5dfd03ecbacedc
maya.marganitt <maya.marganitt@mail.huji.ac.il>
2025-04-19 18:03:10 +0300

    puchi

--- uthreads.cpp
+++ uthreads.cpp
@@ -12,6 +12,7 @@
 #define SETITIMER_ERR "system error: setitimer error"
 #define SPAWN_ERR "thread library error: null _entry_point"
 #define LIMIT_NUM_OF_TRD_ERR "thread library error: maximum number of _threads"
+#define TID_NOT_EXISTS_ERR "thread library error: tid not exists"
 
 /* translate address */
 
@@ -29,17 +30,24 @@ address_t translate_address (address_t addr)
   return ret;
 }
 
+/* States (ENUM) */
+
+enum State {
+    RUNNING, READY, BLOCKED
+};
+
 /* THE THREAD */
 
 class Thread
 {
  private:
-  int _tid{};
-  thread_entry_point _entry_point{};
+  int _tid;
+  thread_entry_point _entry_point;
   char *_stack;
   int _running_quantum_counter;
   address_t _sp;
   address_t _pc;
+  State _state;
 
  public:
   Thread (int tid, thread_entry_point entry_point)
@@ -50,6 +58,12 @@ class Thread
     _running_quantum_counter = 0;
     _sp = (address_t) _stack + STACK_SIZE - sizeof(address_t);
     _pc = (address_t) entry_point;
+    _state = READY;
+  }
+
+  ~Thread() {
+    delete[] _stack;
+
   }
 
   address_t get_sp() {
@@ -58,6 +72,18 @@ class Thread
 
   address_t get_pc() {
     return _pc;
+  }
+
+  int get_quantum_counter() const {
+    return _running_quantum_counter;
+  }
+
+  State get_state() {
+    return _state;
+  }
+
+  void set_state(State state) {
+    _state = state;
   }
 };
 
@@ -78,6 +104,7 @@ class ThreadManager
     _time_per_thread = quantum_usecs;
     _running_thread = 0;
     _quantum_counter = 1;
+    _free_tids[0] = 1;
   }
 
   void start_timer ()
@@ -115,15 +142,54 @@ class ThreadManager
     return tid;
   }
 
-  int setup_thread(tid) {
+  void remove_thread(int tid) {
+    delete &_threads[tid];
+    _threads.erase (tid);
+    _env.erase (tid);
+    _free_tids[tid] = 0;
+  }
 
+  void remove_all() {
+    for (int i=1; i<MAX_THREAD_NUM; i++){
+      if(_free_tids[i]==1) {
+        remove_thread (i+1);
+      }
+    }
+    _env.erase (0);
+  }
+
+  bool is_tid_exists(int tid) {
+    if (tid < 0 || tid >= MAX_THREAD_NUM) {
+      return false;
+    }
+    return _free_tids[tid] == 1;
+  }
+
+  int get_running_tid() const {
+    return _running_thread;
+  }
+
+  int setup_thread() {
+
+  }
+
+  void switch_thread() {
+
+  }
+
+  int get_quantum_counter_of_tid(int tid) {
+    return _threads[tid]->get_quantum_counter();
+  }
+
+  int get_total_quantum_counter() const {
+    return _quantum_counter;
   }
 
  private:
   std::map<int, Thread *> _threads{};
   std::map<int, sigjmp_buf *> _env{};
   std::queue<int> _ready_queue{};
-  int _free_tids[MAX_THREAD_NUM] = {0};
+  std::array<int, MAX_THREAD_NUM> _free_tids = {};
   int _running_thread;
   int _time_per_thread;
   int _quantum_counter;
@@ -138,7 +204,7 @@ class ThreadManager
       if (_free_tids[i] == 0)
       {
         _free_tids[i] = 1;
-        return i + 1;
+        return i;
       }
     }
     return -1;
@@ -172,7 +238,22 @@ int uthread_spawn (thread_entry_point entry_point)
   return manager.add_thread (entry_point);
 }
 
-int uthread_terminate (int tid);
+int uthread_terminate (int tid){
+  if (not manager.is_tid_exists (tid)) {
+    fprintf (stderr, TID_NOT_EXISTS_ERR);
+    return -1;
+  }
+  if (tid == 0) {
+    manager.remove_all();
+    exit(0);
+  }
+  manager.remove_thread (tid);
+  if(manager.get_running_tid() == tid) {
+    manager.switch_thread(1);
+  } else {
+    return 0;
+  }
+}
 
 int uthread_block (int tid);
 
@@ -180,9 +261,19 @@ int uthread_resume (int tid);
 
 int uthread_sleep (int num_quantums);
 
-int uthread_get_tid ();
+int uthread_get_tid () {
+  return manager.get_running_tid();
+}
 
-int uthread_get_total_quantums ();
+int uthread_get_total_quantums () {
+  return manager.get_total_quantum_counter();
+}
 
-int uthread_get_quantums (int tid);
+int uthread_get_quantums (int tid) {
+  if (not manager.is_tid_exists (tid)) {
+    fprintf (stderr, TID_NOT_EXISTS_ERR);
+    return -1;
+  }
+  return manager.get_quantum_counter_of_tid(tid);
+}
 
